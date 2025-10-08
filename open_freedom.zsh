5a0e4ec81ed25d4442ea31fd13aeb533039225a4
bearwithtea <owen.c.mcgrath@icloud.com>
2025-10-07 19:55:19 -0500

    feat: initial file movement

--- open_freedom.zsh
+++ open_freedom.zsh
@@ -1,6 +1,31 @@
+mydir=$(pwd)
+file="open_freedom.zsh"
+filepath="$mydir/$file"
+echo $filepath
+
 # navigate to the root
 cd ~
 cd /Applications
 
 # pass the free app to the open command
 open -n ./Freedom.app
+
+cd $mydir
+echo $mydir
+
+randnum=$((RANDOM % 7))
+
+dirs_to_go=(
+    "/Users/owenmcgrath/Library/Mobile Documents/com~apple~CloudDocs/Home/Extra"
+    "/Users/owenmcgrath/Documents/LTspice/examples"
+    "/Users/owenmcgrath/airflow/logs/dag_processor/2025-09-29"
+    "/Users/owenmcgrath/Movies/TV"
+    "/Users/owenmcgrath"
+    "/Users/owenmcgrath/Public"
+    "/Users/owenmcgrath/Documents/Documents - MacBook"
+)
+
+send_here=$dirs_to_go[$randnum]
+echo $send_here
+
+# mv $send_here
